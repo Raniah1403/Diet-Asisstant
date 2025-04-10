dce089a6d0eb57544d2354ed8d28a517c6cc1a6e
Raniah Maritza <raniahmrz14@gmail.com>
2025-04-10 07:43:32 +0700

    Revisi button panduan pengguna

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,23 @@
 const modal = document.getElementById("helpModal");
-  const btn = document.getElementById("helpBtn");
-  const span = document.getElementsByClassName("close")[0];
+const btn = document.getElementById("helpBtn");
+const span = document.getElementsByClassName("close")[0];
 
-  btn.onclick = function () {
-    modal.style.display = "flex";
-  }
+// Saat tombol ? diklik
+btn.onclick = function () {
+  modal.style.display = "flex";
+  document.body.classList.add("modal-open");
+}
 
-  span.onclick = function () {
+// Saat tanda silang diklik
+span.onclick = function () {
+  modal.style.display = "none";
+  document.body.classList.remove("modal-open");
+}
+
+// Saat klik di luar modal
+window.onclick = function (event) {
+  if (event.target == modal) {
     modal.style.display = "none";
+    document.body.classList.remove("modal-open");
   }
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
-    }
-  }
+}
